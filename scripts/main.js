49f6695e766018b3dff5d66fceba4aa737b06b60
Vajna Péter <vajnapeter91@gmail.com>
2017-05-12 11:44:03 +0200

    JSON berakása, meg jquery a htmlbe

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,2 @@
 'use strict'
+var cofeeDatabase = $.getJSON('https://gist.githubusercontent.com/vanhelzing/1262fade5b44c1f65fb94e3d8bbe49b0/raw/1ff18545b238ca94c04d456a45b35e5866cf82cb/kavek_v4.json')
